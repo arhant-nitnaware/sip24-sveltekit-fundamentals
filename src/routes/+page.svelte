707a37ba2af5c7ace17404c2e87b6af67f125aca
Arhant Nitnaware <arhant00@gmail.com>
2024-07-18 16:59:24 +0530

    final
    made the project by following the tutorial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,27 @@
-<h1>Welcome to Summer Internship 2024</h1>
+<script>
+    export let data;
+</script>
+
+<header class="bg-white py-4 shadow-md sticky top-0 z-100">
+    <div class="container p-4 flex justify-between items-center">
+        <h1 class="text-2xl font-['Arial'] font-bold">
+            craftLab
+        </h1>
+        <a href="/add-post" class="bg-blue-500 rounded px-3 py-2"> + create post </a>
+    </div>
+</header>
+
+<div class="container mx-auto md:my-5">
+    <div class="grid grid-cols-1 my-2 md:grid-cols-3 gap-4">
+        {#each data.posts as post}
+        <!-- post template -->
+        <div>
+            <img class="w-full" src="data:image;base64,{post.image}" alt='post'>
+            <div class="text-sm py-2">
+                <span class="font-bold">{post.username}</span>
+                <span>{post.content}</span>
+            </div>
+        </div>
+        {/each}
+    </div>
+</div>
---
+++ src/routes/add-post/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+    let files = [];
+</script>
+
+<header class="bg-white py-4 shadow-md sticky top-0 z-100">
+    <div class="container p-4 flex justify-between items-center">
+        <h1 class="text-2xl font-['Arial'] font-bold">
+            craftLab
+        </h1>
+        <a href="/" class="bg-blue-500 rounded px-3 py-2">Home</a>
+    </div>
+</header>
+
+<form class="container mx-auto p-5" method="post" enctype="multipart/form-data">
+    <div class="py-3">
+        <label for="dropzone" class="mb-3 flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50">
+            <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                {#if files && files.length}
+                    <p class="text-sm text-gray font-semibold">{files[0].name}</p>    
+                {:else}
+                    <svg class="w-8 h-8 mb-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/></svg>
+                    <p class="text-sm text-gray font-semibold">Click to upload</p>
+                {/if}
+            </div>
+            <input bind:files={files} name="image" id="dropzone" type="file" accept="image/png, image/jpeg" class="hidden" required/>
+        </label>
+
+        <div class="mb-3">
+            <label for="username" class="font-semibold text-gray-800 py-5">username</label>
+            <input name="username" id="username" type="text" class="border bg-gray-200 rounded-sm w-full h-8">
+        </div>
+        
+        <div class="mb-3">
+            <label for="content" class="font-semibold text-gray-800 py-5">description</label>
+            <textarea name="content" id="content" type="text" class="border bg-gray-200 rounded-sm w-full h-20"></textarea>
+        </div>
+        <button type="submit" class="bg-blue-500 rounded px-3 py-2">Share</button>
+    </div>
+</form>
